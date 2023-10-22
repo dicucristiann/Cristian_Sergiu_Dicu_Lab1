337859cd39bbde90e80c7f3a020b052a2f28b46a
dicucristiann <dicucristian24@gmail.com>
2023-10-22 16:55:25 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Cristian_Sergiu_Dicu_Lab1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
